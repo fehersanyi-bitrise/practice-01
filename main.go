6d84b4c829e66849bdbb9b58e1b2216187206939
fehersanyi-bitrise <sandor.feher@bitrise.io>
2019-02-07 19:43:44 +0100

    fizzbuzz works but ugly as hell

--- main.go
+++ main.go
@@ -1,22 +1,45 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
+type num struct {
+	Number int `json:"number"`
+}
+
+//Response is a res
+type Response struct {
+	Response string `json:"response"`
+}
+
 func handleFizzBuzz(w http.ResponseWriter, r *http.Request) {
-	//tbd
-	log.Print(r.Body)
-	log.Print(w.Header())
+	n := num{}
+	err := json.NewDecoder(r.Body).Decode(&n)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//log.Print(n.Number)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	resp := Response{}
+	resp.Response = counter(n.Number)
+	response, err := json.Marshal(resp)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write(response)
 }
 
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/fizzbuzz", handleFizzBuzz).Methods("GET")
-	log.Fatal(http.ListenAndServe(":3030", nil))
+	log.Fatal(http.ListenAndServe(":3030", router))
 
 }
 
@@ -28,6 +51,6 @@ func counter(n int) string {
 	} else if n%3 == 0 {
 		return "fizz"
 	} else {
-		return string(n)
+		return strconv.Itoa(n)
 	}
 }
